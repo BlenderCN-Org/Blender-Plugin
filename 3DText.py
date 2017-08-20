0ac37383581a41317a1f8c13eeb3ed913dfa1ce2
DrawFun <dzf353@gmail.com>
2017-08-20 19:25:06 +1200

    add emission text; adjust the parameters of materials; adjust the default camera position

--- 3DText.py
+++ 3DText.py
@@ -40,7 +40,7 @@ def create_plane():
     my_plane = bpy.data.objects.new("3DText_Plane", my_mesh)
 
     # Set location and scene of object
-    my_plane.location = (-25, -25, -0.5)
+    my_plane.location = (-25, -25, -0.03)
 
     # Create mesh
     my_mesh.from_pydata(verts, [], faces)
@@ -59,7 +59,18 @@ def create_3dtext(text_body):
     text.data.body = text_body
     text.data.extrude = 0.1
     text.data.bevel_depth = 0.02
-    text.data.offset = 0.0
+    text.data.offset = -0.01
+    text.data.align_x = 'CENTER'
+    return text
+
+
+def create_3d_emission_text(text_body):
+    bpy.ops.object.text_add()
+    text = bpy.context.object
+    text.name = "3DText_EMISSION_Text"
+    text.location = (0, 0.1, 0)
+    text.rotation_euler = (math.pi / 2, 0, 0)
+    text.data.body = text_body
     text.data.align_x = 'CENTER'
     return text
 
@@ -80,9 +91,34 @@ def create_3d_text_material():
     for node in nodes:
         nodes.remove(node)
 
-    node = nodes.new(type='ShaderNodeBsdfGlossy')
-    node.inputs['Color'].default_value = [1, 0, 0, 0]
-    node.inputs['Roughness'].default_value = 0.02
+    node = nodes.new(type='ShaderNodeBsdfGlass')
+    node.inputs['Color'].default_value = [0.8, 0.1, 0.18, 0]
+    node.inputs['Roughness'].default_value = 0.2
+
+    node_output = nodes.new(type='ShaderNodeOutputMaterial')
+    links = mat.node_tree.links
+    links.new(node.outputs[0], node_output.inputs[0])
+    return mat
+
+
+def create_3d_text_emission_material():
+    # Create material
+    scn = bpy.context.scene
+    # Set cycles render engine if not selected
+    if not scn.render.engine == 'CYCLES':
+        scn.render.engine = 'CYCLES'
+
+    mat = bpy.data.materials.new("3DText Emission Material")
+    mat.use_nodes = True
+    nodes = mat.node_tree.nodes
+
+    # clear all nodes to start clean
+    for node in nodes:
+        nodes.remove(node)
+
+    node = nodes.new(type='ShaderNodeEmission')
+    node.inputs['Color'].default_value = [1, 1, 1, 0]
+    node.inputs['Strength'].default_value = 1.5
 
     node_output = nodes.new(type='ShaderNodeOutputMaterial')
     links = mat.node_tree.links
@@ -106,11 +142,11 @@ def create_plane_material():
         nodes.remove(node)
 
     diffuse_shader = nodes.new(type='ShaderNodeBsdfDiffuse')
-    diffuse_shader.inputs['Color'].default_value = [0.0, 0.3, 1, 1]
+    diffuse_shader.inputs['Color'].default_value = [0.0, 0.4, 0.8, 1]
     diffuse_shader.inputs['Roughness'].default_value = 0.0
 
-    glass_shader = nodes.new(type='ShaderNodeBsdfGlass')
-    glass_shader.inputs['Color'].default_value = [0.16, 0.16, 0.16, 0]
+    glass_shader = nodes.new(type='ShaderNodeBsdfGlossy')
+    glass_shader.inputs['Color'].default_value = [0.26, 0.26, 0.26, 1]
     glass_shader.inputs['Roughness'].default_value = 0.0
 
     mix_shader = nodes.new(type='ShaderNodeMixShader')
@@ -142,6 +178,12 @@ class Object3DText(bpy.types.Operator):
             text.data.materials[0] = text_material
         else:
             text.data.materials.append(text_material)
+        text_emission = create_3d_emission_text(self.text_body)
+        text_emission_material = create_3d_text_emission_material()
+        if len(text_emission.data.materials):
+            text_emission.data.materials[0] = text_emission_material
+        else:
+            text_emission.data.materials.append(text_emission_material)
         plane = create_plane()
         plane_material = create_plane_material()
         if len(plane.data.materials):
@@ -149,8 +191,8 @@ class Object3DText(bpy.types.Operator):
         else:
             plane.data.materials.append(plane_material)
         camera = bpy.data.objects["Camera"]
-        camera.location = (0, -7, 2)
-        look_at(camera, mathutils.Vector((0, 0, 0)))
+        camera.location = (0, -5, 2)
+        look_at(camera, mathutils.Vector((0, -0, 0)))
         return {'FINISHED'}
 
 
@@ -166,3 +208,6 @@ def unregister():
 
 if __name__ == "__main__":
     register()
+    #camera = bpy.data.objects["Camera"]
+    #camera.location = (0, -5, 2)
+    #look_at(camera, mathutils.Vector((0, -0, 0)))
