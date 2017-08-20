5a302936ed075f4263b69696ab15ed528610f8bf
DrawFun <dzf353@gmail.com>
2017-08-20 16:47:50 +1200

    3d text plugin

---
+++ 3DText.py
@@ -0,0 +1,10 @@
+bl_info = {
+    "name": "3D Text",
+    "author": "DrawFun",
+    "version": (0, 0, 0),
+    "blender": (2, 7, 8),
+    "location" : "View3D > Add > Mesh > 3D Text",
+    "description": "Make 3D Text Effect",
+    "category": "Add Mesh"}
+
+import bpy
